31e2cd263c75bbe322285a8f15a7245ac9f0fa1a
Ashish Goyal <31183326+goyal007@users.noreply.github.com>
2018-12-29 20:12:31 +0530

    Company

---
+++ company.php
@@ -0,0 +1,138 @@
+<?php include('server.php') ?>
+<!doctype html>
+<html>
+<head>
+	<link rel="shortcut icon" type="image/png" href="Mnit_logo.png">
+	<title>Company</title>
+	<link rel="stylesheet" type="text/css" href="stl.css">
+
+</head>
+<body>
+
+<div class="col-sm-12">
+                <div class="logo">
+				<a href="mnit.php"><img src="Mnit_logo.png" alt="MNIT Jaipur"  class="head-logo" height="60"  align="left"/></a>
+			    </div>
+			<h4 class=" tc-black mg-sm">
+					
+					<a class="ltc-black" href="mnit.php">Training &amp; Placement Cell</a>
+				</h4>
+				<h6 class=" mg-clear tc-1">
+					 <a class="pull-left" href="http://www.mnit.ac.in" target="_blank"> Malaviya National Institute of Technology Jaipur</a><br>
+				</h6>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="bloc b-divider l-bloc bgc-white" id="bloc-1">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-sm-6">
+				<ul class="list-unstyled list-horizontal-layout right-align">
+					<div class="navi">
+						<a href="company.php" >Company</a>
+					</div>
+					<div class="navi">
+						<a href="student.php" >Student</a>
+					</div>	
+					<div class="navi">
+		
+						<a href="admin.php" >Admin</a>
+					</div>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+			
+
+
+
+
+
+<div class="company1">
+				<ul class="list-unstyled1 list-horizontal-layout right-align">
+					
+						<a href="company_register.php" class="btn3 ">Company register</a>
+					
+					
+						<a href="company_login.php" class="btn3">Company Login</a>
+					
+				</ul>
+			</div>
+
+
+<div class="hotels">
+				<div class="temp" >
+				<h2>Hotels Near MNIT Jaipur</h2>
+				</div>
+				<div class="column left" style="background-color:whitesmoke;">
+					<h3>Red Fox Hotel</h3>
+					<p>
+						Opp. MNIT,Jawahar Lal Nehru Marg,<br>Bajaj Nagar,Jaipur <br> Rajasthan 302017 India<br> <br><strong>Phone</strong> <br><a href="tel:+911414120101">+91-141-4120101</a>
+					</p>
+				</div>
+				<div class="column middle" style="background-color:whitesmoke;">
+					<h3>Clarks Amer</h3>
+					<p>
+						U.P.Hotels Limited,<br>Jawahar Lal Nehru Marg,<br>Jaipur Rajasthan 302017 India<br> <br><strong>Phone</strong> <br><a href="tel:+911412550616">+91-141-2550616</a>
+					</p>
+				</div>
+				<div class="column right" style="background-color:whitesmoke;">
+					<h3>Treebo Signature Inn </h3>
+					<p>
+						Jawahar Lal Nehru Marg,<br> E-58, Girdhar Marg, Malviya Nagar,<br> Jaipur, Rajasthan 302017 India<br> <br><strong>Phone</strong> <br><a href="tel:+919322800100">+91-9322800100</a
+					</p>
+				</div>
+</div>
+						
+
+
+<div class="bloc b-divider bgc-ghost-white l-bloc" id="bloc-63">
+	<div class="container bloc-md">
+		<div class="row">
+			<div class="col-sm-2">
+				
+			</div>
+			
+			<div class="col-sm-1">
+				<h4 class="mg-md bloc-mob-center-text  tc-dim-gray">
+				
+					<a class="a-btn a-block bloc-mob-center-text" href="http://www.mnit.ac.in/placement/index.php">About Us</a>
+					<a class="a-btn a-block bloc-mob-center-text" href="http://mnit.ac.in/placement/contact.php">Contact Us</a>
+				</h4>
+			</div>
+			
+			
+			<div class="col-sm-44">
+				<h3 class="mg-md  text-right tc-dim-gray">
+					Training &amp; Placement Office
+				</h3>
+				<p class="text-right">
+					Ground Floor,Prabha Bhawan<br>Malaviya National Institute of Technology<br>Jaipur - 302017
+				</p>
+				<p class="footer3">
+					<strong>Phone</strong> <br><a href="tel:+911412529065">+91-141-2529065</a> <br><a href="tel:+919549654241">+91-9549654241</a> 
+				</p>
+				<p class="footer4">
+				<strong>Email</strong> <br><a href="http://www.mnit.ac.in/placement/index.php">placements@mnit.ac.in</a>
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+			
+			
+			
+</body>
+<script type="text/javascript">
+	$("button").click(function() {
+	    $('html,body').animate({
+	        scrollTop: $(".alumni").offset().top},
+	        'slow');
+	});
+</script>
+</html>
